d2631a6b1d3f24d9acc4901c2e89bfe9eff1ff34
Aniket Shelar <theaniketshelar@gmail.com>
2022-08-12 14:05:12 +0530

    Added: employee_daily_wage

--- src/com/intellijide/employeewagecomputation/EmployeeWageBuilder.java
+++ src/com/intellijide/employeewagecomputation/EmployeeWageBuilder.java
@@ -1,13 +1,19 @@
 package com.intellijide.employeewagecomputation;
 
 public class EmployeeWageBuilder {
+    static final int WAGE_PER_HOUR = 20;
+    static final int FULL_DAY_HOUR = 8;
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
         System.out.println("---------------------------------------------");
         int empPresent = (int)Math.floor(Math.random() * 10) % 2;
-        if(empPresent == 1)
+        int empWage = 0;
+        if(empPresent == 1) {
+            empWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
             System.out.println("Employee is Present");
-        else
+        }else
             System.out.println("Employee is Absent");
+
+        System.out.println("Employee Daily Wage => " +empWage);
     }
 }
